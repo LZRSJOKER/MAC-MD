87667a93fd02a5ddbe665f0c7de51ee7e17b65c3
LZRSJOKER <95168467+LZRSJOKER@users.noreply.github.com>
2023-12-16 20:05:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/Maccoder3/MAC-MD'
-global.location = 'Sultanpur IN'
+global.github = 'https://github.com/MYREJOKER'
+global.location = 'HABBIBI'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '256705036288'
-global.devs = '256705036288';
-global.website = 'https://github.com/Maccoder3/MAC-MD' //wa.me/+25000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/8ec95da555e8347af71bd.jpg'
+global.sudo = process.env.SUDO || '917561815867
+global.devs = '917561815867';
+global.website = 'https://github.com/MYREJOKER' //wa.me/+25000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/cbe0b1079b24c77e7dbc9.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'MAC 𝐁𝐨𝐭' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'mac' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '𝘿𝞗𝞜𝙌𝙐𝞘𝞦𝞗𝙏𝞢 𝘿𝞗𝙁𝙇𝞓𝞛𝞘𝞜𝙂𝞗' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '___𝙸𝚃𝚊𝙲𝙷𝙸__🪬' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'mac' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '___𝙸𝚃𝚊𝙲𝙷𝙸__🪬' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'MAC-Md' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '☮️'🕊️ : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
